3f6509dcbd7cce87d0f8f748296cb48a9760e8a1
Cosmos85 <esakesti@gmail.com>
2018-06-28 01:17:53 +0100

    changed from sqlLite to PostgreSQL

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfoliowebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodbb',
+        'USER':'postgres',
+        'PASSWORD':'elju0912',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
